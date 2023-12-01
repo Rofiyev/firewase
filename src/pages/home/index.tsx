1dca430ecd7a8c7c328fbdddf9750323b7bbca09
abbosbek23 <muratovabbosbek@gmail.com>
2023-12-01 10:30:27 +0500

    change box style and add some title

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,6 +5,8 @@ import Layout from "../../layout";
 import home__image from "../../assets/home__image.jpg";
 import { animated } from "react-spring";
 import box_image from "../../assets/Revenue-Accuracy.svg";
+import box_image_2 from "../../assets/Real-Time-Visibility.svg"
+import box_image_3 from "../../assets/Scalable-Revenue-Recognition.svg"
 import { use3dEffect } from "use-3d-effect";
 import partners_1 from "../../assets/partners_1.webp";
 import partners_2 from "../../assets/partners_2.webp";
@@ -122,27 +124,28 @@ export default function Home() {
               </p>
             </div>
             <div className="box">
-              <img src={box_image} alt="" />
-              <p>Revenue Accuracy</p>
+              <img src={box_image_2} alt="" />
+              <p>Real-Time Visibility</p>
               <p>
-                You can`t risk inaccurate revenue accounting. Eliminate errors
-                and minimize audit risk by automating revenue recognition.
-                Streamline sales and revenue data for accurate revenue metrics
-                at your fingertips so you can make decisions that maximize
-                profits and drive growth.
+               Gain a complete picture of your financial health in real-time. Present
+               your financials with confidence knowing you have full visibility into 
+               revenue contract metrics for recognized, planned, and unplanned revenue.
               </p>
             </div>
             <div className="box">
-              <img src={box_image} alt="" />
-              <p>Revenue Accuracy</p>
+              <img src={box_image_3} alt="" />
+              <p>Scalable Revenue Recognition</p>
               <p>
-                You can`t risk inaccurate revenue accounting. Eliminate errors
-                and minimize audit risk by automating revenue recognition.
-                Streamline sales and revenue data for accurate revenue metrics
-                at your fingertips so you can make decisions that maximize
-                profits and drive growth.
+              As your business grows, rev rec gets more complicated. Ensure you can adapt to 
+              current and future revenue models like subscription, usage, services, or a hybrid. 
+              RightRev can process hundreds of millions of transactions for any revenue model in 
+              minutes- performance unmatched by any other solution.
               </p>
             </div>
+          </div>
+          <div style={{textAlign:"center", marginBottom:"50px"}}><button className="home_2_btn">Why RIGHTREV</button></div>
+          <div style={{marginLeft:"10%", width:'83%',padding:"0 ,40px"}}>
+            <h3 className="home_2_under_btn_title">Revenue recognition is becoming more complex. Traditional methods fail to meet you where you are and where you want to go.</h3>
           </div>
         </div>
       </section>
