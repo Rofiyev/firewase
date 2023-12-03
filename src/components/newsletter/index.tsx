d1903559cfcede44f6b389bed0e7876c7eb092ef
Rof1yev <rofiyevdilshod@gmail.com>
2023-12-03 14:07:48 +0500

    heom design change

--- src/components/newsletter/index.tsx
+++ src/components/newsletter/index.tsx
@@ -4,15 +4,20 @@ import "./index.css";
 
 export default function Newsletter() {
   return (
-    <div className="newsletter_component">
-      <h3 className="linear_gradient_title__light">
-        Get out of spreadsheets and workarounds. Get back to Accounting.
-      </h3>
-      <button className="home_2_btn">
-        <Link style={{ color: "inherit", textDecoration: "none" }} to={"/buy"}>
-          Request Demo
-        </Link>
-      </button>
+    <div className="container">
+      <div className="newsletter_component">
+        <h3 className="linear_gradient_title__light">
+          Get out of spreadsheets and workarounds. Get back to Accounting.
+        </h3>
+        <button className="home_2_btn">
+          <Link
+            style={{ color: "inherit", textDecoration: "none" }}
+            to={"/buy"}
+          >
+            Request Demo
+          </Link>
+        </button>
+      </div>
     </div>
   );
 }
