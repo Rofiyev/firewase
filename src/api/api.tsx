eed86b41373fffb052ed60308683917d3726a96f
Rof1yev <rofiyevdilshod@gmail.com>
2023-12-04 17:03:07 +0500

    created fetch functions

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -7,9 +7,41 @@ export const getCategorys = async () => {
     console.log(data);
     return { success: true, data };
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return { success: false, data: [] };
   }
 };
-export const getDocuments = async () => {};
-export const getProducts = async () => {};
+
+export const getDocuments = async (types: string = "") => {
+  const params = {
+    document_type: types,
+  };
+  try {
+    const { data } = await axios.get(`${BASE_URL}/document/`, { params });
+    console.log(data);
+    return { success: true, data };
+  } catch (error) {
+    console.log(error);
+    return { success: false, data: [] };
+  }
+};
+
+export const getProducts = async (
+  product_id: string = "",
+  category_id: string = "",
+  product_last_count: string = ""
+) => {
+  const params = {
+    product_id: product_id,
+    category_id: category_id,
+    product_last_count: product_last_count,
+  };
+  try {
+    const { data } = await axios.get(`${BASE_URL}/product/`, { params });
+    console.log(data);
+    return { success: true, data };
+  } catch (error) {
+    console.log(error);
+    return { success: false, data: [] };
+  }
+};
