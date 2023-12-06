e6372477f40bc3999bcbff2111079a6ee92f7d29
Rof1yev <rofiyevdilshod@gmail.com>
2023-12-06 18:46:35 +0500

    ........

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -27,6 +27,7 @@ export default function Contacts() {
   const [actiteCode, setActive] = useState<string>("");
   const [userCode, setUserCode] = useState<string>("");
   const [disabled, setDisabled] = useState<boolean>(true);
+  const [phoneDisabled, setPhoneDisabled] = useState<boolean>(false);
 
   useEffect(() => window.scrollTo({ top: 0, behavior: "smooth" }), []);
   const { register, handleSubmit, reset } = useForm<Inputs>();
@@ -39,6 +40,7 @@ export default function Contacts() {
     setActive("");
     setUserCode("");
     setDisabled(true);
+    setPhoneDisabled(false);
   };
 
   const handleCode = () => {
@@ -51,6 +53,7 @@ export default function Contacts() {
 
   const handleActivePhone = async () => {
     if (fullName && organiz && tel) {
+      setPhoneDisabled(true);
       const { success, data } = await checkPhoneNumber(tel);
       if (success) {
         setActive(data[tel]);
@@ -175,7 +178,15 @@ export default function Contacts() {
                       setTel(e.target.value)
                     }
                   />
-                  <button className="inputBtn" onClick={handleActivePhone}>
+                  <button
+                    className="inputBtn"
+                    onClick={handleActivePhone}
+                    disabled={phoneDisabled}
+                    style={{
+                      cursor: phoneDisabled ? "no-drop" : "pointer",
+                      opacity: phoneDisabled ? "0.6" : "1",
+                    }}
+                  >
                     Подтвердите
                   </button>
                 </div>
@@ -184,11 +195,13 @@ export default function Contacts() {
                     <input
                       type="number"
                       placeholder="Введите код *"
-                      maxLength={6}
-                      style={{ width: "100%" }}
+                      style={{
+                        width: "100%",
+                      }}
                       onChange={(e: ChangeEvent<HTMLInputElement>) =>
                         setUserCode(e.target.value)
                       }
+                      className="number__input"
                       required
                       autoFocus
                     />
